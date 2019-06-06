09650b2f35bc2eae9ce87acab00d5d986b26161e
tiger00 <834062717@qq.com>
2019-06-07 02:12:21 +0800

    '提交静态页面'

---
+++ src/views/cinema/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="main">
+      <Header title='老虎影院'/>
+      <Tabber/>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/header";
+import Tabber from "@/components/tabber";
+export default {
+  name: "cinema",
+  components: {
+    Header,
+    Tabber
+  }
+};
+</script>
+
+<style scoped>
+</style>
