5afa216ad143d36246ee294c1ebd602da37a27eb
tiger00 <834062717@qq.com>
2019-06-07 14:21:59 +0800

    '静态页面完成'

--- src/views/cinema/index.vue
+++ src/views/cinema/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="main">
       <Header title='老虎影院'/>
+<div id="content">
+			<div class="cinema_menu">
+				<div class="city_switch">
+					全城 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="brand_swtich">
+					品牌 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="feature_switch">
+					特色 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+			</div>
+      <CinemaList/>
+		
+		</div>
+
+
       <Tabber/>
   </div>
 </template>
@@ -8,14 +25,19 @@
 <script>
 import Header from "@/components/header";
 import Tabber from "@/components/tabber";
+import CinemaList from "@/components/cinemaList";
 export default {
   name: "cinema",
   components: {
     Header,
-    Tabber
+    Tabber,
+    CinemaList
   }
 };
 </script>
 
 <style scoped>
+#content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
+#content .cinema_body{ flex:1; overflow:auto;}
+
 </style>
